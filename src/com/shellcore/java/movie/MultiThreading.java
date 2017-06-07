eb5744f3d6ba525168caa72e86f33dee132c086e
Cesar Morales Garduño <shell.one.core@gmail.com>
2017-06-07 07:46:16 -0500

    Inicialización del proyecto, como viene en el ejercicio.

---
+++ src/com/shellcore/java/movie/MultiThreading.java
@@ -0,0 +1,28 @@
+package com.shellcore.java.movie;
+
+import java.util.Scanner;
+
+/**
+ * Created by Cesar. 07/06/2017.
+ */
+public class MultiThreading {
+
+    public static void main(String[] args) {
+        Scanner inputReader = new Scanner(System.in);
+        String userInput;
+
+        String filePath = "./movies.txt";
+        /*Read movies from movies.txt*/
+
+        /*Create clases*/
+
+        /*Start Threads*/
+
+        // Stop with any key, except just Enter
+        while(true) {
+            userInput = inputReader.next();
+            /*End Threads*/
+            System.exit(0);
+        }
+    }
+}
